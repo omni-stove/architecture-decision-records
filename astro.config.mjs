b2dec8317dd69e2ea6a9d7bdd1a64b3084abe8e5
codynog <yoneyoneyone99@gmail.com>
2025-07-16 12:36:03 +0900

    add deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+	output: "static",
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 	integrations: [
 		starlight({
 			title: "Architecture Decision Records",
